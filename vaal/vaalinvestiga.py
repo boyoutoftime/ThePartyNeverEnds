3601c74d9a38508fa474b83318774545f87970c1
boyoutoftime <cristofx1@gmail.com>
2025-06-09 14:57:16 -0600

    Actualizar vaalinvestiga.py

--- vaal/vaalinvestiga.py
+++ vaal/vaalinvestiga.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta, timezone
 os.makedirs("pdfs", exist_ok=True)
 
 subcategorias = [
-    "astro-ph/recent", "cond-mat.mtrl-sci", "gr-qc", "hep-ex",
+    "astro-ph", "cond-mat.mtrl-sci", "gr-qc", "hep-ex",
     "hep-lat", "hep-ph", "hep-th", "math-ph",
     "nlin", "nucl-ex", "nucl-th", "physics.plasm-ph", "quant-ph"
 ]
