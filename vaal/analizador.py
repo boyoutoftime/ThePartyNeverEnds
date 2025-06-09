202a56c0bf70c773e45410aa243aad0cb9fa206e
boyoutoftime <cristofx1@gmail.com>
2025-06-09 15:58:31 -0600

    Actualizar analizador.py

--- vaal/analizador.py
+++ vaal/analizador.py
@@ -33,7 +33,7 @@ class MiniRedNeuronal:
 modelo = MiniRedNeuronal(5, 4, 1)
 
 
-def preprocesar(texto):
+def preprocesar(texto, términos):
     texto = texto.lower()
     texto = re.sub(r'[^\w\s\\\+\-\=\{\}\<\>\.\,\/\:\^\%\°]', '', texto)
     palabras = texto.split()
