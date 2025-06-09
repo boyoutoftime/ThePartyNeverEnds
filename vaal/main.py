aded02da1442384dcf197e308b86443cf10389e3
boyoutoftime <cristofx1@gmail.com>
2025-06-08 22:03:54 -0600

    Actualizar main.py

--- vaal/main.py
+++ vaal/main.py
@@ -20,10 +20,10 @@ class SimpleIAHandler(BaseHTTPRequestHandler):
             return
 
         # Aquí importamos lo necesario
-        from app.buscador import buscar_en_duckduckgo
-        from app.lector import extraer_texto_de_url
-        from app.analizador import analizar_con_pregunta
-        from app.personalidad import dar_estilo
+        from vaal.buscador import buscar_en_duckduckgo
+        from vaal.lector import extraer_texto_de_url
+        from vaal.analizador import analizar_con_pregunta
+        from vaal.personalidad import dar_estilo
 
         try:
             links = buscar_en_duckduckgo(tema)
