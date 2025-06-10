24112c8334485915d11dff2f4817546d9666313f
boyoutoftime <cristofx1@gmail.com>
2025-06-10 17:30:16 -0600

    Actualizar lexer.py

--- vaal/lexer.py
+++ vaal/lexer.py
@@ -20,21 +20,3 @@ def detectar_bloques_latex(texto):
     texto_unido = " ".join(texto_normal).strip()
 
     return texto_unido, ecuaciones
-
-
-# ------------------- Prueba ---------------------
-
-if __name__ == "__main__":
-    texto_prueba = r"""
-Este es un documento sobre física cuántica.
-La ecuación de Schrödinger es: $i\hbar\frac{\partial}{\partial t}\Psi = \hat{H}\Psi$
-También existen expresiones como $E = mc^2$ y otras fórmulas.
-Este texto debe ir como bloque normal.
-"""
-
-    texto_unido, ecuaciones_limpias = detectar_bloques_latex(texto_prueba)
-
-    print("\n→ TEXTO UNIDO:\n", texto_unido)
-    print("\n→ BLOQUES MATEMÁTICOS:")
-    for eq in ecuaciones_limpias:
-        print("-", eq)
